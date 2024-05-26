5309d95b6d7b7306815da5b71b300c4dba51763e
Velin Kurtev <velin.kurtev@dreambox.com>
2024-05-26 03:28:58 +0300

    Saved score

--- src/app/components/book-rater/book-rater.component.ts
+++ src/app/components/book-rater/book-rater.component.ts
@@ -8,33 +8,39 @@ import { Book, DataService } from '../../services/data.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { StarRatingModule } from 'angular-star-rating';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-book-rater',
   standalone: true,
-  imports: [MatCardModule, RatingBarComponent, MatInputModule, FormsModule, MatFormFieldModule, CommonModule, StarRatingModule],
+  imports: [MatCardModule, RatingBarComponent, MatInputModule, FormsModule, MatFormFieldModule, CommonModule, StarRatingModule, MatIconModule, MatButtonModule],
   templateUrl: './book-rater.component.html',
   styleUrl: './book-rater.component.css'
 })
 export class BookRaterComponent {
 
-  name:string;
-  description:string;
-  author:string;
-  rated:boolean = !false;
-  rating:number = 0;
+  name!: string;
+  description!: string;
+  author!: string;
+  editableName!: string;
+  editableDescription!: string;
+  editableAuthor!: string;
+  rated: boolean = false;
+  rating: number = 0;
+  avgRating: number[] = [];
+  totalRating!: number[];
+  bookIndex = 0;
 
   books!: Book[];
   private booksSubscription: Subscription;
-  
+
   constructor(private dataService: DataService) {
     this.booksSubscription = this.dataService.booksData$.subscribe(data => {
       this.books = data;
     });
 
-    this.name = this.books[0].name;
-    this.description = this.books[0].description;
-    this.author = this.books[0].author;
+    this.loadNextBook(this.bookIndex);
   }
 
   ngOnDestroy() {
@@ -45,5 +51,29 @@ export class BookRaterComponent {
 
   onRatingChanged(rating: number) {
     this.rating = rating;
+    this.rated = true;
+    this.editableName = this.name;
+    this.editableDescription = this.description;
+    this.editableAuthor = this.author;
+    this.avgRating[this.bookIndex] = rating;
+  }
+
+  nextBook() {
+    this.rating = 0;
+    this.rated = false;
+    this.bookIndex += 1;
+    this.loadNextBook(this.bookIndex);
+    this.dataService.updateBook(this.bookIndex - 1, { name: this.editableName, description: this.editableDescription, author: this.editableAuthor } as Book)
+  }
+
+  private loadNextBook(index: number) {
+    this.name = this.books[index].name;
+    this.description = this.books[index].description;
+    this.author = this.books[index].author;
+  }
+
+  calculateAverage(arr:number[]) {
+    const sum = arr.reduce((acc, val) => acc + val, 0);
+    return arr.length ? sum / arr.length : 0;
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 export type Book = {
   name: string,
@@ -14,12 +14,28 @@ export type Book = {
 export class DataService {
 
   booksData$: Observable<Book[]> = of([
-    { name: "Test", description: "TestDescription", author: "TestAuthor" }
+    { name: "To Kill a Mockingbird", description: "A novel about the serious issues of rape and racial inequality told through the eyes of young Scout Finch in the Deep South.", author: "Harper Lee" },
+    { name: "1984", description: "A dystopian novel set in a totalitarian society under constant surveillance.", author: "George Orwell" },
+    // { name: "Pride and Prejudice", description: "A romantic novel that critiques the British landed gentry at the end of the 18th century.", author: "Jane Austen" },
+    // { name: "The Great Gatsby", description: "A story about the mysterious millionaire Jay Gatsby and his obsession with Daisy Buchanan.", author: "F. Scott Fitzgerald" },
+    // { name: "Moby-Dick", description: "A narrative of Captain Ahab's obsessive quest to kill the giant white whale, Moby-Dick.", author: "Herman Melville" },
+    // { name: "War and Peace", description: "A historical novel that tells the story of five families during the Napoleonic wars.", author: "Leo Tolstoy" },
+    // { name: "The Catcher in the Rye", description: "A novel about teenage rebellion and angst as narrated by the disenchanted Holden Caulfield.", author: "J.D. Salinger" },
+    // { name: "The Hobbit", description: "A fantasy novel about the journey of Bilbo Baggins, who is swept into an epic quest to reclaim a lost kingdom.", author: "J.R.R. Tolkien" },
+    // { name: "Brave New World", description: "A dystopian novel that explores the dangers of a technologically advanced future society.", author: "Aldous Huxley" },
+    // { name: "Jane Eyre", description: "A novel about the experiences of the orphaned Jane Eyre, including her growth to adulthood and her love for Mr. Rochester.", author: "Charlotte Brontë" }
   ])
 
   constructor() { }
 
-  updateBook(id: number, Book: Book) {
-    // add update book functionality
+  updateBook(id: number, updatedBook: Book) {
+    this.booksData$ = this.booksData$.pipe(
+      map(books => {
+        if (id !== -1) {
+          books[id] = updatedBook;
+        }
+        return books;
+      })
+    );
   }
 }
